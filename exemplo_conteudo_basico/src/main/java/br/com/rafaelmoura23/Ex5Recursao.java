38b8937c82e5ef0339594e8a6043c90daab517e6
rafaelmoura23 <rafaelzinmoura23@gmail.com>
2024-09-19 16:48:57 -0300

    exercicios-basicos

--- exemplo_conteudo_basico/src/main/java/br/com/rafaelmoura23/Ex5Recursao.java
+++ exemplo_conteudo_basico/src/main/java/br/com/rafaelmoura23/Ex5Recursao.java
@@ -1,19 +1,31 @@
 package br.com.rafaelmoura23;
 
+import java.util.Scanner;
+
 public class Ex5Recursao {
-    public static int calcularFatorial(int n) {
-        // Fatorial de 0 ou 1 é igual a 1
+    int numero = -1;
+    Scanner sc = new Scanner(System.in);
+
+    public long calculoFatorial(int n) {
         if (n == 0 || n == 1) {
             return 1;
         } else {
-            // Recursão
-            return n * calcularFatorial(n - 1);
+            return calculoFatorial(n - 1);
         }
     }
 
-    public class ExceptionErro extends Exception {
-        public ExceptionErro(String mensagem) {
-            super(mensagem);
+    public void calculadora() throws Exception{
+        System.out.print("Digite um número:");
+        numero = sc.nextInt();
+
+        if(numero < 0) {
+            throw new Exception("O número deve ser Positivo.");
+        }
+
+        try {
+            System.out.println(calculoFatorial(numero));
+        } catch (Exception e) {
+            System.err.println(e);
         }
     }
 }
--- exemplo_conteudo_basico/src/main/java/br/com/rafaelmoura23/Main.java
+++ exemplo_conteudo_basico/src/main/java/br/com/rafaelmoura23/Main.java
@@ -1,8 +1,9 @@
 package br.com.rafaelmoura23;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
         // new Ex1SistemaNotas().calculosNotas();
-        new Ex3CalculadoraAvancada().calculadora();
+        // new Ex3CalculadoraAvancada().calculadora();
+        new Ex5Recursao().calculadora();
     }
 }
