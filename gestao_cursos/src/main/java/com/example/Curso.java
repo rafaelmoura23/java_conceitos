7e098ad2a60f745bf64f44b54ab54cff9d3c7576
rafaelmoura23 <rafaelzinmoura23@gmail.com>
2024-09-20 16:53:26 -0300

    Exercicios de Conceitos Básicos em Java - Gestão Cursos

--- gestao_cursos/src/main/java/com/example/Curso.java
+++ gestao_cursos/src/main/java/com/example/Curso.java
@@ -8,16 +8,13 @@ public class Curso {
 
 
     public void addAlunos(Aluno aluno){
-
+        
     }
 
 
     public void menu(){
-            System.out.println("1. Soma");
-            System.out.println("2. Subtração");
+            System.out.println("1. Cadastrar Aluno");
+            System.out.println("2. Cadastrar Professor");
             System.out.println("3. Multiplicação");
-            System.out.println("4. Divisão");
-            System.out.println("5. Raiz Quadrada");
-            System.out.println("6. Sair");
     }
 }
